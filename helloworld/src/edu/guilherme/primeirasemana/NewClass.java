6be2c93f4e80591459a087e34e2760ea665ab41e
GuilhermeMLeal <guilherme.mleal.11@gmail.com>
2023-03-21 16:48:54 -0300

    tabela verdade

--- helloworld/src/edu/guilherme/primeirasemana/NewClass.java
+++ helloworld/src/edu/guilherme/primeirasemana/NewClass.java
@@ -2,8 +2,17 @@ package edu.guilherme.primeirasemana;
 
 public class NewClass {
     
+
+    
     public static void main(String[] args) {
         
-    }
-    
+        byte a =1;
+        byte b= 2;
+        
+        boolean simNao = a!=b;
+        System.out.println("Os numeros de a e b são "+ simNao);
+
+       
+
+}
 }
